83ca55986563cb5dbdf05cca11ed1e47344d9761
TraJoe_Wu <traveller_wjoe@sina.com>
2017-03-08 01:28:55 +0800

    请求接口地址前加/api

--- server/router.js
+++ server/router.js
@@ -1,6 +1,6 @@
 const api = require('./api');
 module.exports = router => {
-    router.get('/live', async (ctx, next) => {
+    router.get('/api/live', async (ctx, next) => {
         ctx.body = await api.getLive();
     })
 }
